645c538ea810479cf63fc21c0a56f195940626f6
faiqahfaqihahh <faiqahfaqihahh03@gmail.com>
2024-06-28 20:38:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const changeTextButton = document.getElementById('changeTextButton');
+    changeTextButton.addEventListener('click', () => {
+        changeTextButton.textContent = 'You clicked me!';
+    });
+});
+
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    alert('Welcome to my portfolio website!');
+});
